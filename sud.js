62c3268d7e8ca32d8ced2e56e89c1e4318053fff
VLB771 <62136955+VLB771@users.noreply.github.com>
2020-06-20 17:42:00 +0200

    Add files via upload

--- sud.js
+++ sud.js
@@ -2,39 +2,102 @@ function sudoku(myArg) {
 
 var fs = require('fs');
 var sud = myArg[0];
-
 var sudArr = fs.readFileSync(sud).toString().split("\n");
-for(i in sudArr) {
-    console.log(sudArr[i]);
-}
-
 sumuArr = [];
 for(i in sudArr) {
     sumuArr.push(sudArr[i].split(""));
 }
-console.log(sumuArr);
 
 for(i in sumuArr) {
-    if ((sumuArr[i].includes('_'))) {  
-        str = 45;
+    numArr = [];
+    str = 45;
         for(a in sumuArr[i]){
+            
             if (sumuArr[i][a] !== "_" && sumuArr[i][a] !== "|"){
-                str = str - (sumuArr[i][a]);
+                numArr.push(sumuArr[i][a]); 
+                str = str - (sumuArr[i][a]);   
             }
+            
         }  
-        sumuArr[i] = sumuArr[i].join("").replace("_", str);
-        console.log(sumuArr[i]);
-    }
-    else {
-        sumuArr[i] = sumuArr[i].join("");
-        console.log(sumuArr[i]);  
-    }
-    
+        if (numArr.length === 8){
+
+            sumuArr[i] = sumuArr[i].join("").replace("_", str).split("");
+        }
+                               
 }
 
-console.log("Affichage de la solution:" + "\n" + sumuArr.join("\n"));
+console.log("Affichage après première boucle horizontale:" + "\n");
+for (i in sumuArr) {
+    console.log(sumuArr[i].join(""));
+}
+
+
+for (i in sumuArr){
+    strV = 45;
+
+    for (a in sumuArr[i]){
+        if (sumuArr[i][a] == ("_")){
+
+            r = sumuArr.indexOf(sumuArr[i]);
+            console.log("la rangée " + sumuArr[i] + " est la numéro: " + r);
+            c = sumuArr[i].indexOf(sumuArr[i][a]);
+            console.log("la colonne du chiffre manquant est la numéro: " + c);
+            
+            for(i in sumuArr){
+
+                if (sumuArr[i][c] !== "_" && sumuArr[i][c] !== "-"){
+            
+                    strV = strV - (sumuArr[i][c]); 
+                    
+                } 
+
+            }  
+            console.log("le chiffre manquant est le: " + strV);
+
+            sumuArr[r] = sumuArr[r].join("").replace("_", strV).split("");
+        }else{}
+            
+    }
+
+}
+
+for (i in sumuArr){
+    strV = 45;
+
+    for (a in sumuArr[i]){
+        if (sumuArr[i][a] == ("_")){
+
+            r = sumuArr.indexOf(sumuArr[i]);
+            console.log("la rangée " + sumuArr[i] + " est la numéro: " + r);
+            c = sumuArr[i].indexOf(sumuArr[i][a]);
+            console.log("la colonne du chiffre manquant est la numéro: " + c);
+            
+            for(i in sumuArr){
+
+                if (sumuArr[i][c] !== "_" && sumuArr[i][c] !== "-"){
+            
+                    strV = strV - (sumuArr[i][c]); 
+                    
+                } 
+
+            }  
+            console.log("le chiffre manquant est le: " + strV);
+
+            sumuArr[r] = sumuArr[r].join("").replace("_", strV).split("");
+        }else{}
+            
+    }
+
+}
+
+
+
+console.log("\n" + "Affichage de la solution:" + "\n");
+for (i in sumuArr) {
+    console.log(sumuArr[i].join(""));
+}
 
 };
 
 var myArg = process.argv.slice(2);
- sudoku(myArg) ;
+sudoku(myArg) ;
